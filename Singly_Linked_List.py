f594165c0b15583f2102c61a62c7d30fa29d4640
Sheik <mohamed.sheik.ali1127@zohomail.in>
2022-02-07 11:27:27 +0530

    Linked-List

--- Singly_Linked_List.py
+++ Singly_Linked_List.py
@@ -1,5 +1,8 @@
 # 1) Create a Node.
 
+from hashlib import new
+
+
 class Node:
 
     def __init__(self, data):
@@ -14,6 +17,7 @@ class LinkedList():
     def __init__(self):
         self.head = None
 
+    # Traversing through Linked List
     def printL(self):
         if self.head is None:
             print("Linked List is empty!")
@@ -28,11 +32,23 @@ class LinkedList():
         new_node.next_node = self.head
         self.head = new_node
 
+    def add_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.next_node is not None:
+                n = n.next_node
+            n.next_node = new_node
+
 
 Ll = LinkedList()
 Ll.add_at_beginning(10)
 Ll.add_at_beginning(20)
+Ll.add_at_end(21) # will add at the end of the linked list
 Ll.add_at_beginning(30)
 Ll.add_at_beginning(40)
+Ll.add_at_end(22)
 Ll.add_at_beginning(50)
 Ll.printL()
