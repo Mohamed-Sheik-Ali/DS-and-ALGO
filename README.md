f2f3bfeff1ae105858e85c0863d509215feb0f02
Sheik <mohamed.sheik.ali1127@zohomail.in>
2022-02-08 10:54:21 +0530

    Linked-List

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 <h4>A way to store and organise the data so that it can be accessed effectively.</h4>
 
-<ol>
+<ul>
 <li>Linear Search</li>
 <li>Binary Search</li>
 <li>Stack</li>
 <li>Queue</li>
 <li>Priority Queue</li>
 <li>Linked List</li>
-</ol>
+</ul>
--- Singly_Linked_List.py
+++ Singly_Linked_List.py
@@ -1,11 +1,11 @@
-# 1) Create a Node.
+# Create a Node.
 class Node:
 
     def __init__(self, data):
         self.data = data
         self.next_node = None
 
-# 2) List which contains all the nodes.
+# List which contains all the nodes.
 
 
 class LinkedList():
@@ -20,14 +20,16 @@ class LinkedList():
         else:
             n = self.head
             while n is not None:
-                print(f"{n.data} ->", end="")  
+                print(f"{n.data} ->", end="")
                 n = n.next_node
-    # 3) To add or insert a node in the beginning
+
+    # To add or insert a node in the beginning
     def add_at_beginning(self, data):
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
 
+    # To add or insert a node in the end
     def add_at_end(self, data):
         new_node = Node(data)
         if self.head is None:
@@ -38,12 +40,26 @@ class LinkedList():
                 n = n.next_node
             n.next_node = new_node
 
-    def add_in_between(self, data):
-        new_node = Node(data)
+    # To add a node in-between after a given node(x)
+    def add_after_given_node(self, data, x):
+        n = self.head
+
+        while n is not None:
+            if x == n.data:
+                break
+            n = n.next_node
+        if n is None:
+            print("Node doesn\'t exist")
+        else:
+            new_node = Node(data)
+            new_node.next_node = n.next_node
+            n.next_node = new_node
 
 
 Ll = LinkedList()
 Ll.add_at_beginning(10)
-Ll.add_at_end(21) # will add at the end of the linked list
+Ll.add_at_end(21)  # will add at the end of the linked list
 Ll.add_at_beginning(50)
+Ll.add_after_given_node(100, 10)
+Ll.add_after_given_node(200, 100)
 Ll.printL()
