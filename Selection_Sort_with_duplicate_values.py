4c74b9f857969a21d2d96872d74750d9d02a94a1
Sheik <mohamed.sheik.ali1127@zohomail.in>
2022-02-11 10:29:07 +0530

    Selection-Sort

--- Selection_Sort_with_duplicate_values.py
+++ Selection_Sort_with_duplicate_values.py
@@ -1,2 +1,12 @@
 def Selection_Sort(list):
-    pass
+    for i in range(len(unsorted_list)):
+
+        min_val = min(list[i:])
+        min_val_index = list.index(min_val, i) # where "i" is the start index
+        list[i], list[min_val_index] = list[min_val_index], list[i]
+
+    return list
+
+
+unsorted_list = [77, 2, 88, 22, 1, 29, 0, 77, 21, 0, 1, 22]
+print(Selection_Sort(unsorted_list))
